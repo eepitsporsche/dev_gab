491d20d18ed35330f12cd2f6f1a6f0af93f0593c
Porsche <herskorn@gmail.com>
2024-05-07 19:55:08 -0700

    added cookie settings, middleware, server connection to server.js

--- server.js
+++ server.js
@@ -15,3 +15,37 @@ const PORT = process.env.PORT || 3001;
 
 //Handlebars.JS Engine Helpers
 const hbs = exhbs.create({ helpers });
+
+//Cookies/Session Set-Up
+const sess = {
+    secret: 'SECRET STUFF',
+    cookie: {
+        maxAge: 1200000,
+        httpOnly: true,
+        secure: false,
+        sameSite: 'strict',
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequalizeStore({
+        db: sequelize,
+    }),
+};
+
+app.use(session(sess));
+
+//Handlebars.js Middleware
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+//Parse JSON Data Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+//Initiate Server Connection
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Listening on Port ${PORT}.`));
+});
