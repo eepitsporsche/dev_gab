bf93987ee042d53e9e5bc4a3ed993d9a314cb3b4
Porsche <herskorn@gmail.com>
2024-05-07 19:40:04 -0700

    added dependency imports to server.js and created sequelize environmental variables method

---
+++ config/connection.js
@@ -0,0 +1,20 @@
+//Node Modules
+require('dotenv').config();
+
+const sequelize = require('sequelize');
+
+//Sequelize Import
+const Sequelize = require('sequelize');
+
+//Sequelize Environmental Variables
+const sequelize = process.env.JAWSDB_URL
+    ? new Sequelize(process.env.JAWSDB_URL)
+    : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+        host: 'localhost',
+        dialect: 'mysql',
+        dialectOptions: {
+            decimalNumbers: true,
+        },
+});
+
+module.exports = sequelize
