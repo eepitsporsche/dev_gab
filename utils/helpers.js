ed13f52cdcef0251f67886136821ad920542503b
Porsche <herskorn@gmail.com>
2024-05-09 20:23:54 -0700

    added userAuth helper function

--- utils/helpers.js
+++ utils/helpers.js
@@ -4,5 +4,9 @@ module.exports = {
     //Date Format: MM/DD/YYYY
     format_date: (date) => {
         return date.toLocaleDateString();
-    }
-}
+    },
+    //Verify User ID
+    user_auth: (userLogged, userID) => {
+        return userLogged === userID;
+    },
+};
