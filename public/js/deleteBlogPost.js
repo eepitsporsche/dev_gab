1b8876d7aca8ba8c0cae3679b124f48f1b237859
Porsche <herskorn@gmail.com>
2024-05-09 20:08:34 -0700

    added deleteBlogPost.js functionality

---
+++ public/js/deleteBlogPost.js
@@ -0,0 +1,28 @@
+//
+const deletePost = async (event) => {
+    event.preventDefault();
+    console.log("Click Successful!");
+    console.log(event.target);
+
+    //Split the URL to Pull Out Blog Post ID
+    let blogPost = window.location.pathname.split('/');
+    console.log(blogPost);
+
+    //Pass Blog Post ID Into 'Delete' URL
+    const response = await fetch(`/api/blogPost/${blogPost[2]}`, {
+        method: "DELETE",
+    });
+
+    //Redirect to Blog Dashboard or Respond with Status Code Data
+    if (response.ok) {
+        document.location.replace("/blogDashboard");
+    } else {
+        alert(response.statusText);
+    }
+};
+
+const deleteButton = document.querySelectorAll("#delete_button");
+
+//Loop Over Delete Buttons to Add Event Listeners
+for (let i=0; i<deleteButton.length; i++) {
+    deleteButton[i].addEventListener("click", deletePost)};
