b639b75aaf12574ee37e69e13f283d0e10301e51
Porsche <herskorn@gmail.com>
2024-05-11 10:05:59 -0700

    added router functionality

---
+++ controllers/index.js
@@ -0,0 +1,10 @@
+//Import Dependencies
+const router = require('express').Router();
+const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoutes');
+
+//Middleware
+router.use('./api', apiRoutes);
+router.use('/',homeRoutes);
+
+module.exports = router
