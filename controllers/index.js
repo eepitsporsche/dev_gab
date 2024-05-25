f3570bf5989d6bad2fb4bdb039871b7537b6bb32
Porsche <herskorn@gmail.com>
2024-05-25 11:08:38 -0700

    added dashboard routes

--- controllers/index.js
+++ controllers/index.js
@@ -2,9 +2,11 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
+const dashboardRoutes = require('./dashboardRoutes')
 
 //Middleware
 router.use('/',homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/blogDashboard', dashboardRoutes);
 
 module.exports = router;
