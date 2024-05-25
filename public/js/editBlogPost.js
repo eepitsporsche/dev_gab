40e6a22438a20839030337622071387ea05639f9
Porsche <herskorn@gmail.com>
2024-05-25 11:05:24 -0700

    updated tag path for query selectors

--- public/js/editBlogPost.js
+++ public/js/editBlogPost.js
@@ -1,13 +1,12 @@
-//Edit Blog Post Function
+//Edit Blog Post Form Handler Function
 
-//Pass Blog Post ID Into 'Create' URL
 const editPost = async (event) => {
     event.preventDefault();
 
     //Split the URL to Pull Out Blog Post ID
     const blogPostID = window.location.pathname.split('/')[window.location.toString().split('/').length-1];
-    const blogPostTitle =  document.querySelector('text-area[name="blog_post_title"]').value;
-    const blogPostText = document.querySelector('text-area[name="blog_post_text"]').value;
+    const blogPostTitle =  document.querySelector('input[name="blog_post_title"]').value.trim();
+    const blogPostText = document.querySelector('text-area[name="blog_post_text"]').value.trim();
 
     const response = await fetch(`/api/blogPost/${blogPostID}`, {
         method: 'PUT',
@@ -26,19 +25,3 @@ const editPost = async (event) => {
 };
 
 document.querySelector('.edit_post_form').addEventListener('submit', editPost);
-
-
-    //     console.log("Click Successful!");
-
-//     //*May not need next line of code*
-//     const comment_body = document.getElementById('edit_button').value.trim();
-//     console.log(blogPost);
-
-//     document.location.assign(`/create/${blogPost[2]}`);
-// };
-
-// const editButton = document.querySelectorAll('#edit_button');
-
-// //Loop Over Edit Buttons to Add Event Listeners
-// for (let i=0; i<editButton.length; i++) {
-//     editButton[i].addEventListener("click", editPost)};
