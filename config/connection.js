6dc65d88a2a835f251b8d0fc4420c37040d7750d
Porsche <herskorn@gmail.com>
2024-05-27 20:25:05 -0700

    updated comments

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,17 @@
-//Node Modules
-require('dotenv').config();
-
 //Sequelize Import
 const Sequelize = require('sequelize');
+//Import Sensitive Data from .env
+require('dotenv').config();
 
 //Sequelize Environmental Variables
-const sequelize = process.env.JAWSDB_URL
-    ? new Sequelize(process.env.JAWSDB_URL)
-    : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-        host: 'localhost',
-        dialect: 'mysql',
-        dialectOptions: {
-            decimalNumbers: true,
-        },
-});
+const sequelize = process.env.JAWSDB_URL 
+  ? new Sequelize(process.env.JAWSDB_URL) // JAWSDB_URL is for HEROKU
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, { // process.env is for Local
+    host: 'localhost',
+    dialect: 'mysql',
+    dialectOptions: {
+      decimalNumbers: true,
+    },
+  });
 
 module.exports = sequelize;
