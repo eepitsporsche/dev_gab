bf550e3c4537eeeefb5c80cb43e858bff3295fa7
Porsche <herskorn@gmail.com>
2024-05-27 20:27:54 -0700

    removed dashboard middleware

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,9 @@
-//Import Dependencies
-const router = require('express').Router();
-const apiRoutes = require('./api');
-const homeRoutes = require('./homeRoutes');
-const dashboardRoutes = require('./dashboardRoutes')
+//Import Modules and Routes
+const router = require("express").Router();
+const apiRoutes = require("./api");
+const homeRoutes = require("./home-routes");
 
-//Middleware
-router.use('/',homeRoutes);
-router.use('/api', apiRoutes);
-router.use('/blogDashboard', dashboardRoutes);
+router.use("/api", apiRoutes);
+router.use("/", homeRoutes);
 
 module.exports = router;
